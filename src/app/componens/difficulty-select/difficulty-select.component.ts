d8e67b28f6b1faf3a38639a90ad886c92581d3d8
ASalej <ale-salej@mail.ru>
2017-10-25 08:33:58 +0000

    [+] difficulty-select component

---
+++ src/app/componens/difficulty-select/difficulty-select.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'difficulty-select',
+  templateUrl: '/difficulty-select.component.html'
+})
+
+export class DifficultySelect {
+    public selectedOption: string;
+    public isOptionsVisible: boolean = false;
+    
+    @Input() options: string[] = [];
+    
+    @Output() changeOption = new EventEmitter();
+    
+    public onChangeOption(option: string) {
+        this.changeOption.emit(option);
+        this.selectedOption = option;
+        this.isOptionsVisible = false;
+    }
+    
+    public onShowOptions() {
+        this.isOptionsVisible = true;    
+    }
+    
+}
