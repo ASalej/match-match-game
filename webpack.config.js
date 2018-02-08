f6c99d98dac54427f77830e7cd9aa21974a5ecea
Alena Pazniak <alena.pazniak@thomsonreuters.com>
2018-02-08 19:11:52 +0300

    try to add images

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var WebpackNotifierPlugin = require('webpack-notifier');
 var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -20,23 +21,38 @@ module.exports = {
     devtool: 'source-map',
 
     module: {
+        rules: [
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    }
+                }]
+            }
+        ],
         preLoaders: [
             { exclude: /node_modules/, loader: 'tslint', test: /\.ts$/ }
         ],
         loaders: [
-            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.css$/, loader: "style-loader!css-loader" }
         ]
     },
-    
+
     node: {
       fs: "empty"
     },
 
     plugins: [
-      new WebpackNotifierPlugin(),
-      new webpack.ProvidePlugin({
-          $: "jquery",
-          jQuery: "jquery"
-      }),
+        new WebpackNotifierPlugin(),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+        new CopyWebpackPlugin([
+            { from:'src/app/images', to:'/public/dist/images' }
+        ])
     ]
 };
